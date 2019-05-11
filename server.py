a7caeae045f0ef875db8c4456f2c499a549cac4a
igorzakhar <igorzakhar@gmail.com>
2019-05-11 23:47:03 +0300

    First commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+import aiofiles
+
+
+async def archivate(request):
+    raise NotImplementedError
+
+
+async def handle_index_page(request):
+    async with aiofiles.open('index.html', mode='r') as index_file:
+        index_contents = await index_file.read()
+    return web.Response(text=index_contents, content_type='text/html')
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handle_index_page),
+        web.get('/archive/{archive_hash}/', archivate),
+    ])
+    web.run_app(app)
