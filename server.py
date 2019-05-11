72927d69f46da2667626e3a8d3e1128b371c1919
igorzakhar <igorzakhar@gmail.com>
2019-05-11 23:50:17 +0300

    Add code to zip a file and file transfer

--- server.py
+++ server.py
@@ -1,9 +1,32 @@
+import asyncio
+
 from aiohttp import web
 import aiofiles
 
 
 async def archivate(request):
-    raise NotImplementedError
+    target_dir = request.match_info['archive_hash']
+    cmd = f'zip -r - . -i test_photos/{target_dir}/* -j'
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    resp = web.StreamResponse()
+    resp.headers['Content-Type'] = 'application/zip'
+    resp.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
+
+    await resp.prepare(request)
+
+    chunk_size_bytes = 1
+    while True:
+        archive_chunk = await process.stdout.read(chunk_size_bytes)
+        if not archive_chunk:
+            break
+        await resp.write(archive_chunk)
+
+    return resp
 
 
 async def handle_index_page(request):
